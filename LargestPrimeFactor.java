939838ca1b9da8fec66646b0986157e3725e4092
Sneha2008 <68190893+Sneha2008@users.noreply.github.com>
2021-10-01 09:26:10 +0530

    Add files via upload

---
+++ LargestPrimeFactor.java
@@ -0,0 +1,38 @@
+public class LargestPrimeFactor {
+
+    public static void main(String[] args) {
+
+        long num = 600851475143L;
+        boolean isPrime = true;
+
+        
+        for (int i = 2; i < num; i++) {
+
+
+            
+            if (num % i == 0) {
+
+                for (int j = 2; j < i; j++) {
+
+
+                    if (i % j == 0) {
+
+                        isPrime = false;
+
+                    }
+
+                }
+
+                if (isPrime) {
+
+                    System.out.println(i);
+
+                }
+
+            }   
+
+        }
+
+    }
+
+}
